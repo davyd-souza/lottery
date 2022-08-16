18079ef5c468bcb44a11c8527ef6cf64650d8183
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-08-16 13:08:35 -0300

    feat: implement Lottery component to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 // DEPENDENCY
 import { useState } from 'react';
 
+// COMPONENT
+import { Lottery } from './components/Lottery';
+
 // STYLE
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lottery App</h1>
+      <Lottery />
+      <Lottery title="mini Lotto" numBalls={4} maxNum={10} />
+      <Lottery title="mega Lotto" numBalls={10} maxNum={100} />
     </div>
   );
 }
